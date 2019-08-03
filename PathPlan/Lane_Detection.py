149ac6c5a6b3c6f63cf7d0672813b5572fa04784
Kyungyeon Park <36693953+contestpark@users.noreply.github.com>
2019-08-03 16:32:16 +0900

    new PathPlan structure

---
+++ PathPlan/Lane_Detection.py
@@ -0,0 +1,18 @@
+'''
+#################### PATH PLAN TEAM ####################
+
+## ABOUT
+- 차선을 인식해서 왼쪽차선, 오른쪽차선, 정지선의 위치를 반환해주는 코드
+
+## INPUT & OUTPUT
+- input: 카메라 원
+- output: 각 미션 별 packet
+
+'''
+
+## Lane_Detection.py
+class Lane_Detection:
+    def __init__(self):  # 초기화
+    def left_line(self):  # numpy로 넘겨줌(이미지로)
+    def right_line(self):  # numpy로 넘겨줌(이미지로)
+    def get_stop_line(self):  # 정지선을 반환하는 코드(정지선 제일 앞 부분)
